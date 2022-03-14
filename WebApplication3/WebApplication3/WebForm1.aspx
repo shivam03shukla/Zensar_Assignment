5f11552573f922a89bc1b6a806eb36e094c8b25e
shivam03shukla <shivamshu3@gmail.com>
2022-03-15 00:15:06 +0530

    asp assignment

---
+++ WebApplication3/WebApplication3/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            </asp:DropDownList>
+        </div>
+       select any name: 
+        <br />
+        <br />
+        <asp:Image ID="Image1" runat="server" Height="353px" style="margin-right: 0px" Width="879px" />
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            Check the price of snacks: <asp:Button ID="price" runat="server" Text="price" />
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] str = new string[] { "select", "jalebi", "samosa", "kachori", };
+                for (int i = 0; i < str.Length; i++)
+                {
+                    DropDownList1.Items.Add(str[i]);
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = DropDownList1.Text;
+            Image1.ImageUrl = "~/pics/" + str + ".jpg";
+        }
+        protected void TextBox1_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            TextBox1.Text = DropDownList1.SelectedIndex.ToString();
+            if (DropDownList1.Text == "jalebi")
+            {
+                TextBox1.Text = "Rs 150";
+            }
+            else if (DropDownList1.Text == "kachori")
+            {
+                TextBox1.Text = "Rs 12";
+            }
+
+            else
+            {
+                TextBox1.Text = "Rs 12";
+            }
+
+        }
+
+    }
+}
